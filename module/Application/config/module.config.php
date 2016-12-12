78e73fcce5d7131284aae71d3900002d4b50771a
KobaTatyana <tatjana.koba@privatbank.ua>
2016-12-12 17:29:35 +0300

    first page login route

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -42,8 +42,8 @@ return array(
                     'route'    => '/application',
                     'defaults' => array(
                         '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Index', 'Login',
-                        'action'        => 'index', 'login',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
                     ),
                 ),
                 'may_terminate' => true,
@@ -86,7 +86,7 @@ return array(
     'controllers' => array(
         'invokables' => array(
             'Application\Controller\Index' => Controller\IndexController::class,
-            'Application\Controller\Login' => Controller\LoginController::class
+            'Application\Controller\Login' => Controller\LoginController::class,
         ),
     ),
     'view_manager' => array(
@@ -96,8 +96,10 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml', '/../view/layout/layout_login.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml', '/../view/application/login/login.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/layout_login'     => __DIR__ . '/../view/layout/layout_login.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'application/login/login' => __DIR__ . '/../view/application/login/login.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
